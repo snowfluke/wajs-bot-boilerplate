00410cfa811a399c3165cbfbaf4c8ce101790cda
Awal Ariansyah <awalariansyah7@gmail.com>
2024-08-05 10:28:33 +0700

    move to custom client

--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,15 @@
-import qrcode from "qrcode-terminal";
-import { Client, LocalAuth } from "whatsapp-web.js";
+import { SClient, onReady } from "wwjs-stable-client";
 import { Commander } from "./commander/commander";
 
-const client = new Client({
-  authStrategy: new LocalAuth(),
-  webVersion: "2.3000.1012972578-alpha",
-  webVersionCache: {
-    type: "remote",
-    remotePath:
-      "https://raw.githubusercontent.com/wppconnect-team/wa-version/main/html/{version}.html",
-  },
-  puppeteer: {
-    args: ["--no-sandbox", "--disable-setuid-sandbox"],
-  },
-});
-client.on("qr", (qr) => {
-  qrcode.generate(qr, { small: true });
-});
-
-client.on("ready", () => {
+onReady(async () => {
   console.log("CLIENT IS RUNNING");
 });
 
-client.on("message", (msg) => {
+SClient.on("message", (msg) => {
   Commander(msg);
 });
 
-client.initialize();
+SClient.initialize();
 
 process.once("unhandledRejection", function (reason, p) {
   console.log(reason, p);
